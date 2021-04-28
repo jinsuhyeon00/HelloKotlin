32d66ca9e8e708c0406bbbfe413163f5adf1144d
S-H JIN <jinsuhyeon00@naver.com>
2021-04-28 11:45:22 +0900

    BMI Java & Kotlin

--- app/src/main/java/com/example/hellokotlin/BmiJavaActivit.java
+++ app/src/main/java/com/example/hellokotlin/BmiJavaActivit.java
@@ -3,12 +3,45 @@ package com.example.hellokotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BmiJavaActivit extends AppCompatActivity {
+    EditText heightfield, weightfield;
+    TextView txtResult;
+    Button btnBmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_view_binding);
+
+        heightfield = findViewById(R.id.heightField);
+        weightfield = findViewById(R.id.weightField);
+        txtResult = findViewById(R.id.txtResult);
+        btnBmi = findViewById(R.id.btnBmi);
+
+        btnBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sHeight = heightfield.getText().toString();
+                String sWeight = weightfield.getText().toString();
+   /*             try{
+                    double bmi = Double.parseDouble(sWeight) / Math.pow(Double.parseDouble(sHeight)/100.,2.);
+                    txtResult.setText("Your BMI = " + bmi);
+                }catch (Exception e){
+                    txtResult.setText("inpus(S) missing");
+                }*/
+                // if(!sHeight.equals("") && !sWeight.equals(""))
+                if(!sHeight.isEmpty() && !sWeight.isEmpty()){
+                    double bmi = Double.parseDouble(sWeight) / Math.pow(Double.parseDouble(sHeight) / 100., 2.);
+                    txtResult.setText("Your BMI = " + bmi);
+                }else{
+                    txtResult.setText("inpus(S) missing");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hellokotlin/BmiKotlinActivit.kt
+++ app/src/main/java/com/example/hellokotlin/BmiKotlinActivit.kt
@@ -2,10 +2,40 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.lang.Exception
 
 class BmiKotlinActivit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_view_binding)
+
+        val heightField = findViewById<EditText>(R.id.heightField)
+        val weightField = findViewById<EditText>(R.id.weightField)
+        val btnBmi = findViewById<Button>(R.id.btnBmi)
+        val textResult = findViewById<TextView>(R.id.txtResult)
+
+        btnBmi.setOnClickListener {
+//            val Height = heightField.getText().toString().toDouble()
+//            val Weight = weightField.getText().toString().toDouble()
+
+            val sHeight = heightField.getText().toString()
+            val sWeight = weightField.getText().toString()
+/*            try{
+                val bmi = sWeight.toDouble() / Math.pow(sHeight.toDouble() / 100, 2.0);
+                textResult.setText("Your BMI = " + bmi)
+            }catch (e : Exception){
+                textResult.setText("inpus(S) missing")
+            }
+*/
+            if(!sHeight.equals("") && !sWeight.equals("")){
+                val bmi = sWeight.toDouble() / Math.pow(sHeight.toDouble() / 100, 2.0);
+                textResult.setText("Your BMI = " + bmi)
+            }else{
+                textResult.setText("inpus(S) missing")
+            }
+        }
     }
 }
